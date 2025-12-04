ae2a6d20739ca03c1a9ed391aed181366724a953
CurLeaf <CurLeaf@gmail.com>
2025-12-04 22:40:39 +0800

    init

---
+++ doc/README.md
@@ -0,0 +1,16 @@
+# README
+
+## 安装依赖
+
+### 1. python 环境
+
+```bash
+pip install -r requirements-dev.txt
+```
+
+### 2. 使用Cookiecutter创建Python包项目
+1. 初始化项目模板
+
+```bash
+pip install -r requirements-dev.txt
+```
---
+++ privacy_query_engine/__main__.py
@@ -0,0 +1,67 @@
+# type: ignore[attr-defined]
+from typing import Optional
+
+from enum import Enum
+from random import choice
+
+import typer
+from rich.console import Console
+
+from privacy_query_engine import version
+from privacy_query_engine.example import hello
+
+
+class Color(str, Enum):
+    white = "white"
+    red = "red"
+    cyan = "cyan"
+    magenta = "magenta"
+    yellow = "yellow"
+    green = "green"
+
+
+app = typer.Typer(
+    name="privacy-query-engine",
+    help="A Python SDK for differential privacy and de-identification in SQL queries",
+    add_completion=False,
+)
+console = Console()
+
+
+def version_callback(print_version: bool) -> None:
+    """Print the version of the package."""
+    if print_version:
+        console.print(f"[yellow]privacy-query-engine[/] version: [bold blue]{version}[/]")
+        raise typer.Exit()
+
+
+@app.command(name="")
+def main(
+    name: str = typer.Option(..., help="Person to greet."),
+    color: Optional[Color] = typer.Option(
+        None,
+        "-c",
+        "--color",
+        "--colour",
+        case_sensitive=False,
+        help="Color for print. If not specified then choice will be random.",
+    ),
+    print_version: bool = typer.Option(
+        None,
+        "-v",
+        "--version",
+        callback=version_callback,
+        is_eager=True,
+        help="Prints the version of the privacy-query-engine package.",
+    ),
+) -> None:
+    """Print a greeting with a giving name."""
+    if color is None:
+        color = choice(list(Color))
+
+    greeting: str = hello(name)
+    console.print(f"[bold {color}]{greeting}[/]")
+
+
+if __name__ == "__main__":
+    app()
